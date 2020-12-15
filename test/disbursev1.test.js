fb361d2a3eaf88f9501fb4c0f88ffcacf2513bea
John Matsikas <john.matsikas@gmail.com>
2020-12-15 12:52:29 -0700

    additional tests

--- test/disbursev1.test.js
+++ test/disbursev1.test.js
@@ -1,18 +1,22 @@
 const assert = require('assert');
 const Disburse = artifacts.require("DisburseV1");
 
+var disburse;
+var accounts;
+
 contract("Disburse V1", () => {
 
+    beforeEach(async () => {
+        disburse = await Disburse.deployed();
+        accounts = await web3.eth.getAccounts();
+    });
+
     it("can retrieve empty trust balance", async () => {
-        var disburse = await Disburse.deployed();
-        var accounts = await web3.eth.getAccounts();
         var balance = await disburse.getTrustBalance(accounts[0]);
         assert(balance == 0);
     });
 
     it("can contribute to trust", async () => {
-        var disburse = await Disburse.deployed();
-        var accounts = await web3.eth.getAccounts();
         var weiAmount = web3.utils.toWei('10', 'ether');
         await disburse.contributeToTrust({ from: accounts[0], value: weiAmount });
         var balance = await disburse.getTrustBalance(accounts[0]);
@@ -20,12 +24,39 @@ contract("Disburse V1", () => {
     });
 
     it("can withdraw trust balance", async () => {
-        var disburse = await Disburse.deployed();
-        var accounts = await web3.eth.getAccounts();
         var originalBalance = await web3.eth.getBalance(accounts[0]);
-        await disburse.withdrawTrustBalance({ from: accounts[0]});
+        await disburse.withdrawTrustBalance({from: accounts[0]});
         var newBalance = await web3.eth.getBalance(accounts[0]);
         assert(newBalance >= originalBalance);
+    });
+
+    it("can add beneficiary to trust", async () => {
+        var trustAddress = accounts[0];
+        var beneficiaryAddress = accounts[1];
+        var delayInSeconds = 60;
+        var amount = web3.utils.toWei('5', 'ether');
+        
+        await disburse.addBeneficiarySeconds(beneficiaryAddress, delayInSeconds, amount, {from: trustAddress});
+        var beneficiary = await disburse.getBeneficiary(beneficiaryAddress);
+
+        assert(beneficiary[0] == trustAddress);
+        assert(beneficiary[1] > 0);
+        assert(beneficiary[2] == amount);
+        assert(beneficiary[3] == false);
+    });
+
+    it("cannot add beneficiary without trust", async () => {
+        var trustAddress = accounts[1];
+        var beneficiaryAddress = accounts[2];
+        var delayInSeconds = 60;
+        var amount = web3.utils.toWei('5', 'ether');
+        
+        try {
+            await disburse.addBeneficiarySeconds(beneficiaryAddress, delayInSeconds, amount, {from: trustAddress});
+        }
+        catch(err) {
+            assert(true);
+        }
     });
 
     /*
